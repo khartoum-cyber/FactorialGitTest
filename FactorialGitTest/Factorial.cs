3a13b373dc81d0de8204b5fbcaa2d23b3b8a60fc
bartosz0 <methkill@gmail.com>
2022-01-24 13:51:48 +0100

    added Factorial class

---
+++ FactorialGitTest/Factorial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialGitTest
+{
+    internal class Factorial
+    {
+        public static int FactorialMethod(int i)
+        {
+            Console.WriteLine("Please enter a number to calculate it's factorial : ");
+            var factorial = 1;
+
+            return
+        }
+    }
+}
